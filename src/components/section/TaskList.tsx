1465ce9720e884cee39d4ccd3aae2bc6e87288be
amoljagtap17 <amoljagtap17@gmail.com>
2025-02-09 20:45:16 +0530

    react-19-highlights: add usecallback

--- src/components/section/TaskList.tsx
+++ src/components/section/TaskList.tsx
@@ -1,4 +1,4 @@
-import { Suspense, use } from "react";
+import { Suspense, use, useCallback } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 import { ErrorFallback } from "../lib/ErrorFallback";
 import { useTaskContext } from "./TaskContextProvider";
@@ -26,23 +26,23 @@ function TaskList({ fetchTasks }: ITaskListProps) {
   );
 }
 
+const fetchTasksApi = async () => {
+  const response = await fetch("http://localhost:5000/tasks");
+  const tasks: ITask[] = await response.json();
+
+  return tasks;
+};
+
 export function TaskListContainer() {
   const { updateCount } = useTaskContext();
 
-  console.log("updateCount::", updateCount);
-
-  const fetchTasks = (async () => {
-    const response = await fetch("http://localhost:5000/tasks");
-    const tasks: ITask[] = await response.json();
-
-    return tasks;
-  })();
+  const fetchTasksPromise = useCallback(fetchTasksApi, []);
 
   return (
     <div className="task-list" key={updateCount}>
       <ErrorBoundary FallbackComponent={ErrorFallback}>
         <Suspense fallback={<div>Loading...</div>}>
-          <TaskList fetchTasks={fetchTasks} />
+          <TaskList fetchTasks={fetchTasksPromise()} />
         </Suspense>
       </ErrorBoundary>
     </div>
