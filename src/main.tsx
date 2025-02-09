d3a97d7dcede658cf6fb96b84aa85454b0df1b76
amoljagtap17 <amoljagtap17@gmail.com>
2025-02-09 14:40:38 +0530

    react-19-highlights: add custom font

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import "@fontsource/open-sans/400.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App.tsx";
@@ -6,6 +7,7 @@ import "./index.css";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <div className="container">
+      <h1>Todo App</h1>
       <App />
     </div>
   </StrictMode>
