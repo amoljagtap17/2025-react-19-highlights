2482ca8bac1a002ee68e76a0f2ca90cc0caffd0f
amoljagtap17 <amoljagtap17@gmail.com>
2025-02-09 15:08:01 +0530

    react-19-highlights: use usetransition hook

--- src/components/section/TodoForm.tsx
+++ src/components/section/TodoForm.tsx
@@ -1,25 +1,36 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useTransition } from "react";
 import { Button } from "../lib/Button";
 import { Input } from "../lib/Input";
 import "./styles.css";
 
 export function TodoForm() {
   const inputRef = useRef<HTMLInputElement>(null);
+  const [isPending, startTransition] = useTransition();
 
   useEffect(() => {
     inputRef.current?.focus();
   }, []);
 
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    startTransition(() => {
+      console.log("input::", inputRef.current?.value);
+    });
+  };
+
   return (
     <div className="card">
-      <form>
+      <form onSubmit={handleSubmit}>
         <Input
           name="task"
           label="New Task"
           placeholder="Add New Task"
           ref={inputRef}
         />
-        <Button type="submit">Add</Button>
+        <Button type="submit" disabled={isPending}>
+          Add
+        </Button>
       </form>
     </div>
   );
