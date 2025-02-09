3c3c0ce649ffd9e00b0d96b856ff97b9c1dd9dc0
amoljagtap17 <amoljagtap17@gmail.com>
2025-02-09 14:58:36 +0530

    react-19-highlights: add ref to input

---
+++ src/components/lib/Input.tsx
@@ -0,0 +1,20 @@
+import { useId } from "react";
+import "./input.css";
+
+interface IInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  label: string;
+  name: string;
+  ref: React.RefObject<HTMLInputElement | null>;
+}
+
+export function Input({ ref, label, name, ...props }: IInputProps) {
+  const id = useId();
+  const inputId = `${name}-${id}`;
+
+  return (
+    <div>
+      <label htmlFor={inputId}>{label}</label>
+      <input id={inputId} ref={ref} {...props} />
+    </div>
+  );
+}
--- src/components/section/TodoForm.tsx
+++ src/components/section/TodoForm.tsx
@@ -1,10 +1,23 @@
+import { useEffect, useRef } from "react";
+import { Input } from "../lib/Input";
 import "./styles.css";
 
 export function TodoForm() {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, []);
+
   return (
     <div className="card">
       <form>
-        <input type="text" placeholder="Add a new task" />
+        <Input
+          name="task"
+          label="New Task"
+          placeholder="Add New Task"
+          ref={inputRef}
+        />
         <button type="submit">Add</button>
       </form>
     </div>
